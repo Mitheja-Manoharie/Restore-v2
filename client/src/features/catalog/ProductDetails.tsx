dcbbfe1a888109b4350a37546947af32958a3c56
Mitheja Manoharie <mmanoharie@gmail.com>
2026-07-26 21:13:44 +0530

    End of section 5

---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,97 @@
+import { useParams } from "react-router-dom"
+import type { Product } from "../../app/models/product";
+import { useEffect, useState } from "react";
+import { Button, Divider, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
+import {Grid} from "@mui/material";
+
+export default function ProductDetails() {
+  const {id} = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    fetch(`https://localhost:5001/api/products/${id}`)
+      .then(response => response.json())
+      .then(data => setProduct(data))
+      .catch(error => console.log(error))
+  }, [id]);
+
+  if (!product) return <div>Loading...</div>
+
+  const productDetails = [
+    {label: 'Name', value: product.name},
+    {label: 'Description', value: product.description},
+    {label: 'Type', value: product.type},
+    {label: 'Brand', value: product.brand},
+    {label: 'Quantity in stock', value: product.quantityInStock}
+  ]
+
+  return (
+    <Grid
+    container
+    spacing={6}
+    sx={{
+      maxWidth: "lg",
+      mx: "auto",
+    }}
+  >
+    <Grid size={{ xs: 12, md: 6 }}>
+      <img
+        src={product.pictureUrl}
+        alt={product.name}
+        style={{ width: "100%" }}
+      />
+    </Grid>
+
+    <Grid size={{ xs: 12, md: 6 }}>
+      <Typography variant="h3">
+        {product.name}
+      </Typography>
+
+      <Divider sx={{ mb: 2 }} />
+
+      <Typography variant="h4" color="secondary">
+        ${(product.price / 100).toFixed(2)}
+      </Typography>
+
+      <TableContainer>
+        <Table sx={{
+          '& td': {fontSize: '1rem'}
+        }}>
+          <TableBody>
+            {productDetails.map((detail, index) => (
+              <TableRow key={index}>
+                <TableCell sx={{fontWeight: 'bold'}}>{detail.label}</TableCell>
+                <TableCell>{detail.value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Grid container spacing={2} sx={{ mt: 3 }}>
+        <Grid size={{ xs: 12, sm: 6 }}>
+          <TextField
+            variant="outlined"
+            type="number"
+            label="Quantity in basket"
+            fullWidth
+            defaultValue={1}
+          />
+        </Grid>
+
+        <Grid size={{ xs: 12, sm: 6 }}>
+          <Button
+            sx={{height: '55px'}}
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+          >
+            Add to Basket
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  </Grid>
+  )
+}
